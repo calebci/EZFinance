5021039451271029354790a1cb2ed9cbaef4bf99
calebci <chenci2009@gmail.com>
2023-01-28 17:56:43 -0600

    added budget context and stores data locally

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 import { Button, Stack } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import BudgetCard from './components/BudgetCard';
+
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
-const percentage = 50;
 function App() {
   return (
   <Container className="my-4">
@@ -15,9 +15,6 @@ function App() {
       <Button variant="primary">Add Budget</Button>
       <Button variant="outline-primary">Add Expense</Button>
     </Stack>
-    <p>
-    <CircularProgressbar value={percentage} text={`${percentage}%`} />
-    </p>
     <div style={{
       display: "grid",
       gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
@@ -25,7 +22,7 @@ function App() {
       alignItems: "flex-start",
     }}
     >
-      <BudgetCard name="Entertainment" amount={200} max={1000}></BudgetCard>
+      <BudgetCard name="Entertainment" amount={100} max={1000}></BudgetCard>
     </div>
   </Container>
   )
