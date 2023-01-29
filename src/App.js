deb63a1db6e3a5313bc4a22cfd5abe051021b5af
calebci <chenci2009@gmail.com>
2023-01-28 18:21:48 -0600

    adding budget and expense modals functionality

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Button, Stack } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import BudgetCard from './components/BudgetCard';
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "./contexts/BudgetsContext"
 
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 function App() {
   return (
+  <>
   <Container className="my-4">
     <Stack direction="horizontal" gap="2" className="mb-4">
       <h1 className="me-auto">Budgets</h1>
@@ -25,6 +27,16 @@ function App() {
       <BudgetCard name="Entertainment" amount={100} max={1000}></BudgetCard>
     </div>
   </Container>
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      />
+      <AddExpenseModal
+        show={showAddExpenseModal}
+        defaultBudgetId={addExpenseModalBudgetId}
+        handleClose={() => setShowAddExpenseModal(false)}
+      />
+    </>
   )
 }
 
